bbfe7ca6e3762ce753cdbc7e97a2e0ddbe598af9
Johannes Betz <betz@campus.tu-berlin.de>
2021-01-03 13:50:57 +0100

    fixed day 19 part 1 (rekursively nuilding all messages)

--- code/day_19.py
+++ code/day_19.py
@@ -1,136 +1,97 @@
 import re 
+import itertools
 
-data_string = "day_19_input_test.txt"
+data_string = "day_19_input.txt"
 
 text_file = open(data_string, "r")
 lines = text_file.read()
-lines = lines.split('\n\n')
 
-rulelines = lines[0].split('\n')
+def parseInput(lines):
 
+    lines = lines.split('\n\n')
+    rulelines = lines[0].split('\n')
 
-letter_pattern = re.compile(r'^(")([a-z])(")$')
+    letter_pattern = re.compile(r'^(")([a-z])(")$')
+    rules = {}
 
+    # rules have either numbers or letters
+    for rule in rulelines:
 
-rules = {}
-a = 'a'
-b = 'b'
+        # Get rid of leading id in rule string
+        ID, rule = int(rule.split(': ')[0]), rule.split(': ')[1]
 
-# rules have either numbers or letters
-for rule in rulelines:
-    
+        # single letters ("a"-"z")
+        if match := re.search(letter_pattern, rule):
+            letter = match.group()[1:-1]
+            rules[ID] = letter
 
-    # Get rid of leading id in rule string
-    ID, rule = int(rule.split(': ')[0]), rule.split(': ')[1]
-
-    # single letters ("a"-"z")
-    if match := re.search(letter_pattern, rule):
-        letter = match.group()[1:-1]
-        rules[ID] = letter
-
-    # One or two possible sets of numbers
-    else:
-        numbers = rule.split()
-        if '|' in numbers:
-            subrules = rule.split(' | ')
-            subrules = [list(map(int, number.split())) for number in subrules] 
+        # One or two possible sets of numbers
         else:
-            subrules = rule.split()
-            subrules = [int(number) for number in subrules]
+            numbers = rule.split()
+            if '|' in numbers:
+                subrules = rule.split(' | ')
+                subrules = [list(map(int, number.split())) for number in subrules] 
+            else:
+                subrules = rule.split()
+                subrules = [[int(number) for number in subrules]]
 
-        rules[ID] = subrules
+            rules[ID] = subrules
+
+    messages = lines[1].split('\n')
+
+    return rules, messages
 
 
-#any(isinstance(listitems, list) for listitems in list1)
+# Recursive search for all valid messages, memorizing IDs that we have seen before
+def resolve_rules(rules, mem={}, ID=0):
 
-
-messages = lines[1].split('\n')
-
-
-def replaceValinTuple(oldTuple, index, values):
-    newList = list(oldTuple)
-    firstHalf = newList[:index]
-    secondHalf = newList[index+1:]
-
-    if type(values) != list:
-        values = [values]
-
-    newList = firstHalf + values + secondHalf
+    # ID seen before
+    if ID in mem:
+        return mem[ID]
     
+    # Current rule
+    rule = rules[ID]
 
-    return tuple(newList)
+    # Basecase: single letter
+    if isinstance(rule, str):
+        mem[ID] = rule
+        return rule
 
-test = (1,2,3)
-value = [2,3]
-test = replaceValinTuple(test, 1, value)
-print(test)
-print()
-def allMatchings(rules,  matchings):
+    out = []
+    # Go through all subrules; subrule = [[ID1, ID2, ID3]] or [[ID11, ID21, ID31],[ID12, ID22, ID32]] 
+    for subrule in rule:
+        
+        #next_rules = [resolve_rules(rules, mem, next_i) for next_i in subrule] # NOTE: this does the same, but shorter
+        next_rules= []
+        for next_ID in subrule:
+            next_rule = resolve_rules(rules, mem, next_ID)
+            next_rules.append(next_rule)
 
-    # macthings :set = {(a n1,...,ni, b), (...), (...)}
-    for match in matchings:
-        for char_idx, character in enumerate(match):
+        #print('next_rules', next_rules)
+        #out.extend("".join(x) for x in itertools.product(*next_rules)) # NOTE: this does the same, but shorter
+        for x in itertools.product(*next_rules): # itertool can produce all permutations in *temp
             
-            # more subrules
-            if type(character) == int:
-
-                # retrieve string of rules with current ID character
-                subrules = rules[character]
-
-                # Multiple Subrules 
-                if any(isinstance(listitems, list) for listitems in subrules):
-                    
-                    # put new rules in matching                     
-                    firstrules  = subrules[0]
-                    secondrules = subrules[1]
-
-                    newmatch1 = match
-                    newmatch2 = match
-
-                    newmatch1 = replaceValinTuple(newmatch1, char_idx, firstrules)
-                    newmatch2 = replaceValinTuple(newmatch2, char_idx, secondrules)
-
-                    matchings.remove(match)
-                    matchings.add(newmatch1)
-                    matchings.add(newmatch2)
-
-                # Single Subrules
-                else:
-                    newmatch = match
-                    newmatch = replaceValinTuple(newmatch, char_idx, subrules)
-
-                    matchings.remove(match)
-
-
-                    matchings.add(newmatch)
-                    
-                    
-                # TODO: recursive function calls
-                    matchings = allMatchings(rules,  matchings)
-
-
-            # letter digits are "ready"  
-            else: 
-                pass
-
-    return matchings
-
-
+            #print(xx)
+            X = ["".join(x)]
+            out.extend(X)
     
+    print('ID={} -> mem{}'.format(ID, out))
+    mem[ID] = out
+    return out
 
+# Parse Input data
+# rules: List[List[Int], str] 
+# messages: List[str]
+rules, messages = parseInput(lines)
 
 # Part 1: How many messages match rule 1
+validMessages = set(resolve_rules(rules))
+
 numMachtesRule0 = 0
-ZERO = {tuple(rules[0])}
-matches = allMatchings(rules, ZERO)
+
 for message in messages:
-
-    if message in matches:
+    if message in validMessages:
         numMachtesRule0 += 1
-    
-        
 
-
-
-print(numMachtesRule0, 'messages match rule', ID)
+print(numMachtesRule0, 'messages match rule', 0)
 
